f624b5a11e583b9ce8f56a0bead36d01492bc1da
Guilherme Lima <guilhermelimaesilva.p@gmail.com>
2023-09-23 10:03:28 -0300

    Add: Layout da barra de pesquisa e tabela

---
+++ Guilherme_Lima/script.js
@@ -0,0 +1,7 @@
+"name": "Tonya Ritchie",
+"email": "Jasmin84@yahoo.com",
+"address": "12700 Renner Pine",
+"city": "Cathrynland",
+"state": "KS",
+"cep": "21734-8270",
+"phoneNumber": "808-948-6506"
